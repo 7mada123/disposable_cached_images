c9f22011416ca67b76c28de4ac6b91fb71b8db7a
7mada123 <mohronaldo0@gmail.com>
2022-04-05 04:14:01 +0200

    Update image_widgets.dart

--- example/lib/image_widgets.dart
+++ example/lib/image_widgets.dart
@@ -21,25 +21,31 @@ class ImageWidget extends StatelessWidget {
           ),
         ),
       ),
-      child: DisposableCachedImage.network(
-        imageUrl: imageUrl,
-        fit: BoxFit.cover,
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
-        onLoading: (context, height, width) => const Center(
-          child: Icon(Icons.downloading),
-        ),
-        onError: (context, error, stackTrace, retryCall) => Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(error.toString()),
-              const SizedBox(height: 10),
-              IconButton(
-                onPressed: retryCall,
-                icon: const Icon(Icons.download),
-              ),
-            ],
+      child: Hero(
+        tag: imageUrl,
+        child: DisposableCachedImage.network(
+          imageUrl: imageUrl,
+          width: MediaQuery.of(context).size.width * 0.5,
+          // Resize the image to this width to reduce raster thread usage
+          resizeImage: true,
+          fit: BoxFit.cover,
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          onLoading: (context, height, width) => const Center(
+            child: Icon(Icons.downloading),
+          ),
+          onError: (context, error, stackTrace, retryCall) => Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(error.toString()),
+                const SizedBox(height: 10),
+                IconButton(
+                  onPressed: retryCall,
+                  icon: const Icon(Icons.download),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -51,9 +57,8 @@ class ImageViewScreen extends StatelessWidget {
   final String imageUrl;
 
   const ImageViewScreen({
-    Key? key,
     required this.imageUrl,
-  }) : super(key: key);
+  }) : super(key: const Key('view_screen'));
 
   @override
   Widget build(BuildContext context) {
@@ -62,15 +67,19 @@ class ImageViewScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Center(
-          child: DisposableCachedImage.network(
-            imageUrl: imageUrl,
-            fit: BoxFit.fitWidth,
-            shape: BoxShape.circle,
-            onLoading: (context, height, width) => const Center(
-              child: Icon(Icons.downloading),
-            ),
-            onError: (context, error, stackTrace, retryCall) => Text(
-              error.toString(),
+          child: Hero(
+            tag: imageUrl,
+            child: DisposableCachedImage.network(
+              imageUrl: imageUrl,
+              width: MediaQuery.of(context).size.width,
+              onLoading: (context, height, width) => const Center(
+                child: Icon(Icons.downloading),
+              ),
+              onError: (context, error, stackTrace, retryCall) {
+                return Text(
+                  error.toString(),
+                );
+              },
             ),
           ),
         ),
