5ae25c715b6300baf4cdf3f49e3cd6f4b18773c7
Mohammed Alfateh <67849255+7mada123@users.noreply.github.com>
2022-10-02 21:26:30 +0400

    new way to initialize

--- lib/run_app_wrapper.dart
+++ lib/run_app_wrapper.dart
@@ -8,24 +8,66 @@ part of disposable_cached_images;
 /// {@endtemplate}
 ///
 /// `enableWebCache` Enable or disable web caching
+final _imageStorage = ImageStorageManger.getPlatformInstance();
+
+@Deprecated('use [DisposableImages]')
+
+/// ```dart
+/// Future<void> main() async {
+///   WidgetsFlutterBinding.ensureInitialized();
+///
+///   await DisposableImages.init(enableWebCache: false);
+///
+///   runApp(const DisposableImages(MyApp()));
+/// }
+/// ```
 Future<void> runAppWithDisposableCachedImage(
   final Widget app, {
   final List<Override> overrides = const [],
   final List<ProviderObserver>? observers,
   final bool enableWebCache = true,
 }) async {
-  final cache = ImageCacheManger.getPlatformInstance();
-
-  await cache.init(enableWebCache);
+  await _imageStorage.init(enableWebCache);
 
   runApp(
     ProviderScope(
       overrides: [
-        imageDataBaseProvider.overrideWithValue(cache),
         ...overrides,
       ],
       observers: observers,
       child: app,
     ),
   );
+}
+// TODO
+// running on web html renderer
+// html.document.body?.getAttribute("flt-renderer")?.contains("html")
+
+class DisposableImages extends StatelessWidget {
+  const DisposableImages(
+    this.child, {
+    super.key,
+    this.overrides = const [],
+    this.observers,
+  });
+
+  final List<Override> overrides;
+  final List<ProviderObserver>? observers;
+
+  final Widget child;
+
+  static Future<void> init({final bool enableWebCache = true}) {
+    return _imageStorage.init(enableWebCache);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      overrides: [
+        ...overrides,
+      ],
+      observers: observers,
+      child: child,
+    );
+  }
 }
